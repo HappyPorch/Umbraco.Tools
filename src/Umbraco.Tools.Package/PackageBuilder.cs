ddb9360f8c0c7ed758181968d676c8595cc83a4e
Ondrej Pialek <ondrej@pialek.eu>
2015-06-04 13:47:26 +0800

    Initial project with a single tool that can generate Umbraco packages

---
+++ src/Umbraco.Tools.Package/PackageBuilder.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Umbraco.Tools.Package
+{
+    public class PackageBuilder : IDisposable
+    {
+        private ZipArchive archive;
+        private XDocument packageFile;
+
+        public PackageBuilder(string packageFileName, string destination)
+        {
+            var stream = new FileStream(destination, FileMode.Create);
+            archive = new ZipArchive(stream, ZipArchiveMode.Create, false);
+
+            packageFile = XDocument.Load(packageFileName);
+        }
+
+        public void AddDll(string filename)
+        {
+            var file = new FileInfo(filename);
+            if (!file.Exists)
+                throw new InvalidOperationException($"The requested file {filename} does not exist");
+
+            using (var fileStream = file.Open(FileMode.Open))
+            {
+                AddFile(file.Name, "/bin", fileStream);
+            } 
+        }
+
+        public void AddBackofficeFile(string filename)
+        {
+            var file = new FileInfo(filename);
+            if (!file.Exists)
+                throw new InvalidOperationException($"The requested file {filename} does not exist");
+
+            var destination = GetBackOfficeFileDestinationDir(file);
+            using (var fileStream = file.Open(FileMode.Open))
+            {
+                AddFile(file.Name, destination, fileStream);
+            }
+        }
+
+        private string GetBackOfficeFileDestinationDir(FileInfo file)
+        {
+            var dirs = new Stack<string>();
+            var currentDir = file.Directory;
+            while (currentDir.Name != "App_Plugins")
+            {
+                dirs.Push(currentDir.Name);
+                currentDir = currentDir.Parent;
+            }
+            var sb = new StringBuilder();
+            sb.Append("/App_Plugins");
+            while (dirs.Count > 0)
+            {
+                sb.Append("/");
+                sb.Append(dirs.Pop());  
+            }
+
+            return sb.ToString();
+        }
+
+        private void AddFile(string filename, string destinationDir, Stream fileStream)
+        {
+            var filesNode = packageFile.Descendants("files").First();
+            filesNode.Add(
+                new XElement("file",
+                    new XElement("guid", filename),
+                    new XElement("orgPath", destinationDir),
+                    new XElement("orgName", filename)
+            ));
+
+            AddEntryToArchive(filename, fileStream.CopyTo);
+        }
+
+        private void AddEntryToArchive(string entryName, Action<Stream> streamAction)
+        {
+            var entry = archive.CreateEntry(entryName, CompressionLevel.Optimal);
+            using (var entryStream = entry.Open())
+            {
+                streamAction(entryStream);
+            }
+        }
+
+        public void Done()
+        {
+            AddEntryToArchive("Package.xml", packageFile.Save);
+        }
+
+        public void Dispose()
+        {
+            ((IDisposable)archive).Dispose();
+        }
+    }
+}
---
+++ src/Umbraco.Tools.Package/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Framework.ConfigurationModel;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Serialization;
+
+namespace Umbraco.Tools.Package
+{
+    public class ApplicationSettings
+    {
+        public string[] Dlls { get; set; }
+        public string PackageXmlTemplate { get; set; }
+        public string PluginFolder { get; set; }
+    }
+
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine(@"
+  ______ _   _ _____ __________  _   _ ______      
+ |  ____| \ | |  __ \___  / __ \| \ | |  ____|     
+ | |__  |  \| | |  | | / / |  | |  \| | |__        
+ |  __| | . ` | |  | |/ /| |  | | . ` |  __|       
+ | |____| |\  | |__| / /_| |__| | |\  | |____      
+ |______|_| \_|_____/_____\____/|_| \_|______|___  
+ | |  | |  \/  |  _ \|  __ \     /\   / ____/ __ \ 
+ | |  | | \  / | |_) | |__) |   /  \ | |   | |  | |
+ | |  | | |\/| |  _ <|  _  /   / /\ \| |   | |  | |
+ | |__| | |  | | |_) | | \ \  / ____ \ |___| |__| |
+  \____/|_|  |_|____/|_|  \_\/_/    \_\_____\____/ 
+                                                   
+                                                  
+");
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine(@"Usage: package settings.json 
+Output: a zip file with the package for Umbraco
+settings.json structure:
+{
+    dlls: ['DLL1', 'DLL2'],
+    packageXmlTemplate: 'PATH_TO_PACKAGE.XML'
+    pluginFolder: 'PATH_TO_PLUGIN_FOLDER'
+}");
+                Console.ReadLine();
+                return;
+            }
+
+            var configText = File.OpenText(args[0]).ReadToEnd();
+            var config = (ApplicationSettings)JsonConvert.DeserializeObject(configText, typeof (ApplicationSettings), new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            });
+
+            Console.WriteLine("Dlls: " + string.Join(",", config.Dlls));
+            Console.WriteLine("PackageXmlTemplate: " + config.PackageXmlTemplate);
+            Console.WriteLine("PluginFolder: " + config.PluginFolder);
+
+            Console.WriteLine();
+            Console.WriteLine("Processing...");
+            Console.WriteLine();
+
+            using (var builder = new PackageBuilder(config.PackageXmlTemplate, "Package.zip"))
+            {
+                foreach (var dll in config.Dlls)
+                {
+                    Console.WriteLine($"Adding {dll}.");
+                    builder.AddDll(dll);
+                }
+
+                var directory = new DirectoryInfo(config.PluginFolder);
+                var files = directory.GetFiles("*.*", SearchOption.AllDirectories);
+
+                foreach (var file in files)
+                {
+                    Console.WriteLine($"Adding {file.FullName}.");
+                    builder.AddBackofficeFile(file.FullName);
+                }
+
+                builder.Done();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("All done");
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
