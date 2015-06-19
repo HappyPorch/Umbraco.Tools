38954a870f889a864758a54731f93128027d3f5a
Ondrej Pialek <ondrej@pialek.eu>
2015-06-19 18:22:10 +0800

    replace connection string in web config after db migration accept server instance as a param redone the sync so that it works on my machine (sync app pool info)

--- src/Umbraco.Tools.Sync/sync.ps1
+++ src/Umbraco.Tools.Sync/sync.ps1
@@ -11,12 +11,22 @@
     [Parameter(Mandatory=$True)]
     [string]$destinationServer, 
 
-    [switch]$changeDbFromServerToFile,
+    [switch]$syncDb,
 
+    [string]$databaseServer,
+
+    [Parameter(Mandatory=$True)]
     [string]$remoteUserName, 
 
+    [Parameter(Mandatory=$True)]
     [string]$remotePassword
 )
+
+
+if ($syncDb -eq $True -and -not $databaseServer) {
+    throw "Db sync was requested but database server instance was not specified. Please provide a value for `databaseServer` paramter."
+}
+
 
 function Ensure-WDPowerShellMode {
 	$WDPowerShellSnapin = Get-PSSnapin -Name WDeploySnapin3.0 -ErrorAction:SilentlyContinue
@@ -149,17 +159,18 @@ function Start-SiteAndPool($localSite) {
 function Sync-Sites {
 	try {
         $hostname = "$destinationSiteName.$destinationServer" 
+        $physicalPath = "$destinationRootPath\$destinationSiteName"
 
 		Write-Host " - Syncing site '$siteName' to $hostname..." -NoNewline
 
-        $Result = Sync-WDSite -ErrorAction:Stop `
-            -SourceSite $siteName `
-            -DestinationSite $destinationSiteName `
-            -SitePhysicalPath "$destinationRootPath\$destinationSiteName" `
-            -SourcePublishSettings source.publishsettings `
-            -DestinationPublishSettings destination.publishsettings `
-            -SiteBinding "*:80:$hostname" `
-            -IncludeAppPool
+        & "C:\Program Files\IIS\Microsoft Web Deploy V3\msdeploy.exe" -verb:sync -verbose `
+            -source:appHostConfig=`'$siteName`' `
+            -dest:appHostConfig=`'$destinationSiteName`',computername=$destinationServer,username=$remoteUserName,password=$remotePassword `
+            -skip:objectName=environmentVariables `
+            -skip:objectName=binding `
+            -enableLink:AppPoolExtension `
+            -replace:objectName=virtualDirectory,scopeAttributeName=physicalPath,match=`'^.*:\\.*$`',replace=`'$physicalPath`' `
+            -postSync:runCommand="`"%windir%\system32\inetsrv\appcmd.exe set site /site.name:$destinationSiteName /+bindings.[protocol='http',bindingInformation='*:80:$hostname']`"" 
 			
 	} catch {
 		$exception = $_.Exception
@@ -168,14 +179,13 @@ function Sync-Sites {
 	}
 	
 	Write-Host "OK" -ForegroundColor Green
-	Write-Host "Summary:" -NoNewline
-	$Result | Out-String
+    return $physicalPath
 }
 
 function Sync-Dbs ($localSite) {
 
 	try {
-        $destinationConnectionString = "Data Source=.\SQLEXPRESS;Initial Catalog=$destinationSiteName;Integrated Security=True"
+        $destinationConnectionString = "Data Source=$databaseServer;Initial Catalog=$destinationSiteName;Integrated Security=True"
 
 		Write-Host " - Syncing db to '$destinationServer' as ($destinationConnectionString)..." -NoNewline
 
@@ -188,9 +198,44 @@ function Sync-Dbs ($localSite) {
         $Result = Sync-WDSQLDatabase -ErrorAction:Stop `
             -SourceDatabase $sourceConnectionString `
             -DestinationDatabase "$destinationConnectionString" `
-            -DestinationSettings @{ dropDestinationDatabase = $True } `
-            -SourcePublishSettings source.publishsettings `
             -DestinationPublishSettings destination.publishsettings
+
+	} catch {
+		Write-Host "ERROR" -ForegroundColor:Red
+		throw $_.Exception
+	}
+	
+	Write-Host "OK" -ForegroundColor Green
+	Write-Host "Summary:" -NoNewline
+	$Result | Out-String
+
+    return $destinationConnectionString
+}
+
+function Replace-ConnectionString($siteFolder, $connectionString) {
+    try {
+
+		Write-Host " - Replacing the db conection string on the far side..." -NoNewline
+
+        $webConfigFilename = "$siteFolder\Web.config"
+        
+        $script = "& {
+            [xml]`$xml = Get-Content `"$webConfigFilename`"
+            `$connectionStringNode = `$xml.configuration.connectionStrings.add | ? { `$_.name -eq `"umbracoDbDSN`" }
+            `$connectionStringNode.connectionString = `"$connectionString`"
+            `$xml.Save(`"$webConfigFilename`") 
+        }"
+
+        $bytes = [System.Text.Encoding]::Unicode.GetBytes($script)
+        $encodedCommand = [Convert]::ToBase64String($bytes)
+
+        $command = "powershell.exe -encodedCommand $encodedCommand"
+
+        $Result = Invoke-WDCommand $command `
+            -ErrorAction:Stop `
+            -SourcePublishSettings source.publishsettings `
+            -DestinationPublishSettings destination.publishsettings `
+            -DestinationSettings @{ dontUseCommandExe = $True }
 		
 	} catch {
 		$exception = $_.Exception
@@ -201,30 +246,28 @@ function Sync-Dbs ($localSite) {
 	Write-Host "OK" -ForegroundColor Green
 	Write-Host "Summary:" -NoNewline
 	$Result | Out-String
+
 }
 
 function Set-Permissions($localSite) {
 	try {
-		Write-Host " - Setting folder permissions for '$siteName' on '$destinationServer'..." -NoNewline
+		Write-Host " - Setting folder permissions for '$destinationSiteName' on '$destinationServer'..." -NoNewline
 
         $appPoolName = $localSite.applicationPool
         $rights = [System.Security.AccessControl.FileSystemRights]"ListDirectory,ReadData,Traverse,ExecuteFile,ReadAttributes,ReadPermissions,Read,ReadAndExecute,Modify,Write"
 
-        $Result = Set-WDAcl -ErrorAction:Stop `
+        Set-WDAcl -ErrorAction:Stop `
             -DestinationPublishSettings destination.publishsettings `
             -Destination $destinationSiteName `
             -SetAclUser "IIS AppPool\$appPoolName" `
-            -SetAclAccess $rights
+            -SetAclAccess $rights | Out-Null
 
 	} catch {
-		$exception = $_.Exception
 		Write-Host "ERROR" -ForegroundColor:Red
-		throw " - Set-WDAcl failed: $exception"
+		throw $_.Exception
 	}
 	
 	Write-Host "OK" -ForegroundColor Green
-	Write-Host "Summary:" -NoNewline
-	$Result | Out-String
 }
 
 try {
@@ -237,9 +280,10 @@ try {
     $site = Get-Site
     try {
         Stop-SiteAndPool $site
-        Sync-Sites
-        if ($changeDbFromServerToFile) {
-            Sync-Dbs $site -ErrorAction:Stop
+        $remotePhysicalPath = Sync-Sites
+        if ($syncDb) {
+            $remoteConnectionString = Sync-Dbs $site -ErrorAction:Stop
+            Replace-ConnectionString $remotePhysicalPath $remoteConnectionString 
         }
     } finally {
         Start-SiteAndPool $site
