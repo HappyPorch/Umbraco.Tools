404f2dfa5812cc8b499e85baf29bb746434d4763
Ondrej Pialek <ondrej@pialek.eu>
2015-06-24 12:41:57 +0700

    Added examples showing how to use the sync scripts

---
+++ src/Umbraco.Tools.Sync/example - from elevated PS with DB.ps1
@@ -0,0 +1,1 @@
+.\sync.ps1 -siteName "localSite" -destinationRootPath "C:\sites" -destinationSiteName "remoteSiteName" -destinationServer "remote-server.com" -syncDb -databaseServer ".\SQLEXPRESS" -remoteUserName "Administrator" -remotePassword "SECURE_PWD"
---
+++ src/Umbraco.Tools.Sync/example - self elevating with DB.cmd
@@ -0,0 +1,6 @@
+@ECHO OFF
+SET ThisScriptsDirectory=%~dp0
+SET PowerShellScriptPath=%ThisScriptsDirectory%sync.ps1
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File """"%PowerShellScriptPath%"""" -siteName """"localSiteName"""" -destinationSiteName """"remoteSiteName"""" -destinationRootPath """"C:\inetpub\wwwroot\sites"""" -destinationServer """"remote-server.com"""" -syncDb -databaseServer="""".\SQLEXPRESS"""" -remoteUserName """"Administrator"""" -remotePassword """"SECRET_PWD"""" ' -Verb RunAs}";
+
